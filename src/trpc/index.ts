6192a95be27eb52b5965b7f7080b4a443bc535b3
Kapil <kapilsoni54768161@gmail.com>
2023-12-26 22:40:54 +0530

    Added the product related backend and fetched all products in the frontend

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,8 +1,53 @@
+import { z } from "zod";
 import { authRouter } from "./auth-router";
 import { publicProcedure, router } from "./trpc";
+import { QueryValidator } from "../lib/validators/query-validator";
+import { getPayloadClient } from "../get-payload";
 
 export const appRouter = router({
   auth: authRouter,
+  getInfiniteProduct: publicProcedure
+    .input(
+      z.object({
+        limit: z.number().min(1).max(100),
+        cursor: z.number().nullish(),
+        query: QueryValidator,
+      })
+    )
+    .query(async ({ input }) => {
+      const { query, cursor } = input;
+      const { sort, limit, ...quertOpts } = query;
+      const payload = await getPayloadClient();
+      const parsedQueryOpts: Record<string, { equals: string }> = {};
+      Object.entries(quertOpts).forEach(([key, value]) => {
+        parsedQueryOpts[key] = {
+          equals: value,
+        };
+      });
+      const page = cursor || 1;
+      const {
+        docs: items,
+        hasNextPage,
+        nextPage,
+      } = await payload.find({
+        collection: "products",
+        where: {
+          approvedForSale: {
+            equals: "approved",
+          },
+          ...parsedQueryOpts,
+        },
+        sort,
+        depth: 1,
+        limit,
+        page,
+      });
+
+      return {
+        items,
+        nextPage: hasNextPage ? nextPage : null,
+      };
+    }),
 });
 
 export type AppRouter = typeof appRouter;
